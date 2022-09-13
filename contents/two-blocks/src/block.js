ca08c9764fe1ae3ac4f0dffb343b22750c365899
ph-moura <phmlinux@gmail.com>
2022-09-13 14:39:26 -0300

    Update na classe 'Block'
    Bug-fixing e inserção de novos métodos.

--- contents/two-blocks/src/block.js
+++ contents/two-blocks/src/block.js
@@ -1,24 +1,35 @@
 class Block {
-    constructor(p, w, h, m, color = "blue") {
+    constructor(p, w, h) {
+        this.color = "gray";
+        this.height = h;
+        this.mass = 1.0;
         this.pos = p;
         this.width = w;
-        this.height = h;
-        this.mass = m;
         this.acceleration = new Vector2D(0, 0);
-        this.velocity = new Vector2D(0, 0);
-        this.weight = Vector2D.scale(gravity, this.mass);
-        this.normal = new Vector2D(0, 0);
         this.forces = new Vector2D(0, 0);
-        this.fillStyle = color;
+        this.normal = new Vector2D(0, 0);
+        this.velocity = new Vector2D(0, 0);
+        this.weight = new Vector2D.scale(gravity, this.mass);
     }
 
     move() {
         this.acceleration = Vector2D.scale(this.forces, 1.0 / this.mass);
-        this.pos.incrementBy(this.velocity);
+        this.pos.incrementBy(Vector2D.scale(this.velocity, 0.001));
         this.velocity.incrementBy(this.acceleration);
     }
 
+    getAcceleration() {
+        this.acceleration = Vector2D.scale(this.forces, 1.0 / this.mass);
+        return this.acceleration;
+    }
+
+    setMass(m) {
+        this.mass = m;
+        this.weight = new Vector2D.scale(gravity, this.mass);
+    }
+
     draw(ctx) {
+        this.fillStyle = this.color;
         this.move();
         ctx.save();
         ctx.translate(this.pos.x, this.pos.y);
