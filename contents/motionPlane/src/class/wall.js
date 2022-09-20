d50ccf80c9efcc86b5a736e6b1882fd7b573998b
PROPedroRibeiro <propedroribeiro@gmail.com>
2022-09-20 17:54:56 -0300

    New class to construct walls that can handle collisions with balls.

---
+++ contents/motionPlane/src/class/wall.js
@@ -0,0 +1,20 @@
+class Wall{
+    constructor(x1, y1, x2, y2){
+        this.start = new Vector2D(x1, y1);
+        this.end = new Vector2D(x2, y2);
+        WALLZ.push(this);
+    }
+
+    drawWall(){
+        ctx.beginPath();
+        ctx.moveTo(this.start.x, this.start.y);
+        ctx.lineTo(this.end.x, this.end.y);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    wallUnit(){
+        return Vector2D.norma(Vector2D.subtract(this.end,this.start));
+    }
+}
