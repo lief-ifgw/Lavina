f75c20e31332fa73c564c34cea040a61f330e8e7
ph-moura <phmlinux@gmail.com>
2022-09-20 15:26:14 -0300

    Não sobrescrever constantes.

--- contents/two-blocks/src/block.js
+++ contents/two-blocks/src/block.js
@@ -1,10 +1,11 @@
 class Block {
-    constructor(p, w, h) {
-        this.color = "gray";
+    constructor(p, w, h, c = "gray") {
+        this.color = c;
         this.height = h;
         this.mass = 1.0;
-        this.pos = p;
+        this.pos = new Vector2D(p.x, p.y);
         this.width = w;
+        this.gravity = new Vector2D(0,10);
         this.acceleration = new Vector2D(0, 0);
         this.forces = new Vector2D(0, 0);
         this.normal = new Vector2D(0, 0);
@@ -13,19 +14,29 @@ class Block {
     }
 
     move() {
-        this.acceleration = Vector2D.scale(this.forces, 1.0 / this.mass);
-        this.pos.incrementBy(Vector2D.scale(this.velocity, 0.001));
+        this.acceleration = new Vector2D.scale(this.forces, 1.0 / this.mass);
+        this.pos.incrementBy(new Vector2D.scale(this.velocity, 0.001));
         this.velocity.incrementBy(this.acceleration);
     }
 
     getAcceleration() {
-        this.acceleration = Vector2D.scale(this.forces, 1.0 / this.mass);
+        this.acceleration = new Vector2D.scale(this.forces, 1.0 / this.mass);
         return this.acceleration;
     }
 
     setMass(m) {
         this.mass = m;
-        this.weight = new Vector2D.scale(gravity, this.mass);
+        this.weight = new Vector2D.scale(this.gravity, this.mass);
+    }
+
+    resetForces() {
+        this.forces = new Vector2D(0, 0);
+        this.weight = new Vector2D.scale(0, 0);
+        this.acceleration = new Vector2D(0, 0);
+    }
+
+    setPos(p) {
+        this.pos = new Vector2D(p.x, p.y);
     }
 
     draw(ctx) {
