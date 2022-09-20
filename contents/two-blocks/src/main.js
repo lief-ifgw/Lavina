79ed98d04f66bdac1a194bbf29a189c7e18cd927
ph-moura <phmlinux@gmail.com>
2022-09-20 15:29:23 -0300

    Update:
    1) Mudança de variáveis globais para variáveis locais;
    2) Criação da função 'init' para inicializar a simulação;
    3) Prevenção de sobrescrita de constantes nos objetos.

--- contents/two-blocks/src/main.js
+++ contents/two-blocks/src/main.js
@@ -14,7 +14,6 @@ const defaultForce = 15;
 const defaultMass1 = 2.5;
 const defaultMass2 = 1.5;
 const gravity = new Vector2D(0, 10);
-const nullForce = new Vector2D(0, 0);
 const pos1 = new Vector2D(0, canvasHeight - b1Height);
 const pos2 = new Vector2D(b1Width - b2Width, canvasHeight - b1Height - b2Height);
 
@@ -26,13 +25,9 @@ canvas.height = canvasHeight;
 var animId;
 var b1;
 var b2;
-var force;
-var kFriction1;
-var kFriction2;
-var normal21;
-var sFriction1Max;
-var sFriction2Max;
-var totalMass;
+var forceVector = new Arrow({x:0, y:0}, 0, 0);
+var posForce = new Vector2D(b1Width, canvasHeight - b1Height/2);
+var running = false;
 
 var btnReset = document.getElementById("btnReset");
 var btnRun = document.getElementById("btnRun");
@@ -49,6 +44,8 @@ massView2.innerHTML = sliderMass2.value;
 
 sliderForce.oninput = function() {
     forceView.innerHTML = this.value;
+    forceVector = new Arrow(posForce, 2.5 * sliderForce.value,0);
+    draw();
 }
 
 sliderMass1.oninput = function() {
@@ -59,12 +56,29 @@ sliderMass2.oninput = function() {
     massView2.innerHTML = this.value;
 }
 
-btnRun.onclick = function() {
+function init() {
+    b1 = new Block(pos1, b1Width, b1Height, b1Color);
+    b2 = new Block(pos2, b2Width, b2Height, b2Color);
+
     b1.setMass(1.0 * sliderMass1.value);
     b2.setMass(1.0 * sliderMass2.value);
-    force = new Vector2D(1.0 * sliderForce.value, 0);
-    dynamics();
-    animate();
+
+    forceVector = new Arrow(posForce, 2.5 * sliderForce.value, 0);
+}
+
+document.onload = init();
+
+btnRun.onclick = function() {
+    if(!running) {
+        if (answered === true) {
+            init();
+            dynamics(1.0 * sliderForce.value);
+            running = true;
+            animate();
+        } else {
+            mustAnswer();
+        }
+    }
 }
 
 btnReset.onclick = function() {
@@ -75,34 +89,30 @@ btnReset.onclick = function() {
     massView1.innerHTML = sliderMass1.value;
     massView2.innerHTML = sliderMass2.value;
 
-    dynamics();
+    running = false;
+    posForce = new Vector2D(pos1.x + b1Width, pos1.y + b1Height/2);
+    forceVector = new Arrow(posForce, 2.5 * defaultForce,0);
+
+    init();
     draw();
     pause();
 }
 
-b1 = new Block(pos1, b1Width, b1Height);
-b1.color = b1Color;
-
-b2 = new Block(pos2, b2Width, b2Height);
-b2.color = b2Color;
-
-function forces() {
-    b2.normal = Vector2D.inverse(b2.weight);
-    b2.forces.incrementBy(Vector2D.add(b2.weight, b2.normal));
-    normal21 = Vector2D.inverse(b2.normal);
-    b1.normal = Vector2D.inverse(Vector2D.add(b1.weight, normal21));
+function dynamics(f) {
+    b2.normal = new Vector2D.inverse(b2.weight);
+    b2.forces.incrementBy(new Vector2D.add(b2.weight, b2.normal));
+    let normal21 = new Vector2D.inverse(b2.normal);
+    b1.normal = new Vector2D.inverse(new Vector2D.add(b1.weight, normal21));
     b1.forces.incrementBy(b1.weight);
     b1.forces.incrementBy(b1.normal);
     b1.forces.incrementBy(normal21);
-    sFriction1Max = cs1 * b1.normal.length();
-    sFriction2Max = cs2 * b2.normal.length();
-    kFriction1 = new Vector2D(-1.0 * ck1 * b1.normal.length(), 0.0);
-    kFriction2 = new Vector2D(ck2 * b2.normal.length(), 0.0);
-}
 
-function dynamics() {
-    forces();
-    totalMass = b1.mass + b2.mass;
+    let totalMass = b1.mass + b2.mass;
+    let sFriction1Max = cs1 * b1.normal.length();
+    let sFriction2Max = cs2 * b2.normal.length();
+    let kFriction1 = new Vector2D(-1.0 * ck1 * b1.normal.length(), 0.0);
+    let kFriction2 = new Vector2D(ck2 * b2.normal.length(), 0.0);
+    let force = new Vector2D(f, 0);
 
     if (force.length() > sFriction1Max) {
         b1.forces.incrementBy(force);
@@ -116,14 +126,20 @@ function dynamics() {
             b2.forces = new Vector2D(b2.mass * acc, 0.0);
         } else {
             b2.forces.incrementBy(kFriction2);
-            b1.forces.incrementBy(Vector2D.scale(kFriction2, -1.0));
+            b1.forces.incrementBy(new Vector2D.scale(kFriction2, -1.0));
         }
     }
 }
 
 function move() {
-    b2.move();
-    b1.move();
+    if(b1.pos.x >= b2.pos.x || b1.pos.x >= canvasWidth - 1.5 * b1Width) {
+        pause();
+    } else {
+        b2.move();
+        b1.move();
+        posForce = new Vector2D(b1.pos.x + b1Width, b1.pos.y + b1Height/2);
+        forceVector = new Arrow(posForce, 2.5 * sliderForce.value,0);
+    }
 }
 
 function pause() {
@@ -135,13 +151,14 @@ function draw() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     b1.draw(ctx);
     b2.draw(ctx);
+    forceVector.draw(ctx);
     ctx.restore();
 }
 
 function animate() {
+    animId = requestAnimationFrame(animate);
     move();
     draw();
-    animId = requestAnimationFrame(animate);
 }
 
 draw();
