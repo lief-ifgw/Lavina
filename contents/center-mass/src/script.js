c07b6ba6904824c977b15a33d3644cf59257bef9
PROPedroRibeiro <propedroribeiro@gmail.com>
2022-10-05 00:52:05 -0300

    Removida a checkbox e suas funcionalidades, que foram substituidas pelo botão de iniciar. Programação do botão alterada. Dados do centro de massa reorganizados para facilitar a inserção dos dados no formato LaTeX.

--- contents/center-mass/src/script.js
+++ contents/center-mass/src/script.js
@@ -7,8 +7,9 @@ canvas.height = canvasHeight;
 
 var ctx = canvas.getContext('2d');
 
-
-let checkboxCM  = document.getElementById("displayCM");
+let displayCM  = document.getElementById("displayCM");
+let resetCM = document.getElementById("resetCM");
+let answerCM = document.getElementById("answerCM");
 let ball1X = document.getElementById("b1x");
 let ball1Y = document.getElementById("b1y");
 let ball1M = document.getElementById("mb1");
@@ -24,8 +25,11 @@ let r3 = document.getElementById("r3");
 let opt1 = document.getElementById("opt1");
 let opt2 = document.getElementById("opt2");
 let opt3 = document.getElementById("opt3");
+let divCM = document.getElementById("cm");
+let ansXCM = document.getElementById("ansXCM");
+let ansYCM = document.getElementById("ansYCM");
+let ansCM = document.getElementById("ansCM");
 
-checkboxCM.checked = false;
 let x = 100;
 let y = 100;
 let k = 0;
@@ -34,6 +38,7 @@ let xcm = 0;
 let ycm = 0;
 let gridX = 30;
 let gridY = 30;
+let clicked;
 const BALLZ = [];
 const WALLZ = [];
 const MASS = [];
@@ -117,33 +122,48 @@ let ball3 = new Ball("ball3", Math.random()*canvasWidth, Math.random()*canvasHei
 
 
 drawGrid(gridX,gridY,'blue');
-BALLZ.forEach((b, index) => {
+BALLZ.forEach((b) => {
     b.drawBall();
 });
 
 massCenter();
 
+
 let xcm0 = round(xcm - pex * s, 0);
 let ycm0 = round(-1 * ycm + pey * (Math.round(ySpaces)-s), 0);
 
-let arrCMX = [xcm0-1-Math.round(Math.random()*10),xcm0+Math.round(Math.random()*10),xcm0];
-let arrCMY = [ycm0-Math.round(Math.random()*10),ycm0+Math.round(Math.random()*10),ycm0];
+let arrCMX = [xcm0-Math.ceil(Math.random()*10),xcm0+Math.ceil(Math.random()*10),xcm0];
+let arrCMY = [ycm0-Math.ceil(Math.random()*10),ycm0+Math.ceil(Math.random()*10),ycm0];
 let arrCM = ["X:"+arrCMX[0]+", Y:"+arrCMY[0],"X:"+arrCMX[1]+", Y:"+arrCMY[1],"X:"+arrCMX[2]+", Y:"+arrCMY[2]];
 arrCM = arrCM.sort(() => Math.random() - 0.5)
-
-ball1X.innerHTML = round(ball1.pos.x - pex * s, 0);
-ball1Y.innerHTML = round(-1 * ball1.pos.y + pey * (Math.round(ySpaces)-s), 0);
-ball1M.innerHTML = ball1.m;
-ball2X.innerHTML = round(ball2.pos.x - pex * s, 0);
-ball2Y.innerHTML = round(-1 * ball2.pos.y + pey * (Math.round(ySpaces)-s), 0);
-ball2M.innerHTML = ball2.m;
-ball3X.innerHTML = round(ball3.pos.x - pex * s, 0);
-ball3Y.innerHTML = round(-1 * ball3.pos.y + pey * (Math.round(ySpaces)-s), 0);
-ball3M.innerHTML = ball3.m;
+let ball1x = round(ball1.pos.x - pex * s, 0);
+let ball2x = round(ball2.pos.x - pex * s, 0);
+let ball3x = round(ball3.pos.x - pex * s, 0);
+let ball1y = round(-1 * ball1.pos.y + pey * (Math.round(ySpaces)-s), 0); 
+let ball2y = round(-1 * ball2.pos.y + pey * (Math.round(ySpaces)-s), 0);
+let ball3y = round(-1 * ball3.pos.y + pey * (Math.round(ySpaces)-s), 0);
+let ball1m = ball1.m;
+let ball2m = ball2.m;
+let ball3m = ball3.m;
+let sumBallx = ball1x * ball1m + ball2x * ball2m + ball3x * ball3m;
+let sumBally = ball1y * ball1m + ball2y * ball2m + ball3y * ball3m;
+let sumMass = ball1m + ball2m + ball3m;
+ball1X.innerHTML = ball1x;
+ball2X.innerHTML = ball2x;
+ball3X.innerHTML = ball3x;
+ball1Y.innerHTML = ball1y;
+ball2Y.innerHTML = ball2y;
+ball3Y.innerHTML = ball3y;
+ball1M.innerHTML = ball1m;
+ball2M.innerHTML = ball2m;
+ball3M.innerHTML = ball3m;
 r1.innerHTML = arrCM[0];
 r2.innerHTML = arrCM[1];
 r3.innerHTML = arrCM[2];
 
+ansXCM.innerHTML = "$\\LARGE{x_{cm} = \\frac{("+ball1x+")*("+ball1m+")+("+ball2x+")*("+ball2m+")+("+ball3x+")*("+ball3m+")}{"+ball1m+"+"+ball2m+"+"+ball3m+"}=\\frac{"+sumBallx+"}{"+sumMass+"}\\approx "+xcm0+"}$";
+ansYCM.innerHTML = "$\\LARGE{y_{cm} = \\frac{("+ball1y+")*("+ball1m+")+("+ball2y+")*("+ball2m+")+("+ball3y+")*("+ball3m+")}{"+ball1m+"+"+ball2m+"+"+ball3m+"}=\\frac{"+sumBally+"}{"+sumMass+"}\\approx "+ycm0+"}$";
+ansCM.innerHTML = "$\\large{("+xcm0+","+ycm0+")}$";
 
 function isAnswered() {
     if (opt1.checked === true || opt2.checked === true || opt3.checked === true) {
@@ -162,51 +182,59 @@ function cleanAnswer() {
 
 function mustAnswer() {
     window.alert("Você deve marcar uma opção para ver o resultado.");
-    checkboxCM.checked = false;
-}
-
-function cm(){
-    if(isAnswered()){
-        if(checkboxCM.checked){
-            drawX(xcm,ycm,20,7);   
-            document.getElementById("cmX").innerHTML = xcm0;
-            document.getElementById("cmY").innerHTML = ycm0; 
-            let t = "X:"+xcm0+", Y:"+ycm0;
-            for(i = 0; i < arrCM.length; i++){
-                if(t === arrCM[i]){
-                    if(t === r1.innerHTML){
-                        r1.className = "correct";
-                        r2.className = "wrong";
-                        r3.className = "wrong";
-                    }
-                    else if(t === r2.innerHTML){
-                        r1.className = "wrong";
-                        r2.className = "correct";
-                        r3.className = "wrong";
-                    }
-                    else if(t === r3.innerHTML){
-                        r1.className = "wrong";
-                        r2.className = "wrong";
-                        r3.className = "correct";
-                    }
-                }
-            }
-        }
-        else{
-            ctx.clearRect(0,0,canvasWidth,canvasHeight);
-            drawGrid(gridX, gridY,'blue');
-            BALLZ.forEach((b, index) => {
-                b.drawBall();
-            });
-            document.getElementById("cmX").innerHTML = '';
-            document.getElementById("cmY").innerHTML = ''; 
-        }
-        document.getElementById("myCanvas").focus();
-    }
-    else{
-        mustAnswer();
-        
-    }
 }
 
  cleanAnswer();
+
+ displayCM.onclick = function() {
+    if (isAnswered()) {
+        clicked = true;
+        drawX(xcm,ycm,20,7);   
+        divCM.classList.remove('hide');
+        document.getElementById("cmX").innerHTML = xcm0;
+        document.getElementById("cmY").innerHTML = ycm0; 
+        let t = "X:"+xcm0+", Y:"+ycm0;
+        for(i = 0; i < arrCM.length; i++){
+            if(t === arrCM[i]){
+                if(t === r1.innerHTML){
+                    r1.className = "correct";
+                    r2.className = "wrong";
+                    r3.className = "wrong";
+                }
+                else if(t === r2.innerHTML){
+                    r1.className = "wrong";
+                    r2.className = "correct";
+                    r3.className = "wrong";
+                }
+                else if(t === r3.innerHTML){
+                    r1.className = "wrong";
+                    r2.className = "wrong";
+                    r3.className = "correct";
+                }
+            }
+        }
+    document.getElementById("myCanvas").focus();
+    }
+    else{
+        mustAnswer();
+    }
+}
+
+resetCM.onclick = function() {
+    document.location.reload();
+}
+
+answerCM.addEventListener("click", function() {
+    if(clicked){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    }
+    else{
+        mustAnswer();
+    }
+});
