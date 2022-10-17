1dc56631447650fa7525de7f5a07eae056c2f7c8
Mateus Corradini Lopes <64249204+MateusCorradini@users.noreply.github.com>
2022-10-17 14:52:48 -0300

    Arrumando os desenhos do pêndulo.

--- contents/Pendulo/src/main.js
+++ contents/Pendulo/src/main.js
@@ -1,11 +1,14 @@
 /********* Dimensions ************/
 const canvasWidth   = 600;
 const canvasHeight  = 400;
+const pivoX         = canvasWidth / 2.0;  // X position for the pivot
+const pivoY         = 0;    // '' for the Y
 
 /********** Variables ********/
 
 var theta, compL;         // Initial angle and rope lenght
 var ang_freq;             // Oscillation period and angular frequency (to be answered)
+var xpos, ypos, radians;  // Pendulum position parameters
 
 /********** Physical constants ********/
 
@@ -41,40 +44,38 @@ sliderLenght.oninput = function() {
     lenghtView.innerHTML = this.value;
     ang_freq = Math.sqrt(defaultG/sliderLenght.value);
     omegaView.innerHTML = ang_freq;
+    draw();
+
+
 }
 
 
 sliderAngle.oninput = function() {
     angleView.innerHTML = this.value;
+    draw();
 }
 
 /********** Animation ********/
 
-ctx.beginPath();
-ctx.moveTo(300,0);
-ctx.lineTo(300,200);
-ctx.arc(300, 200, 20, 0, 2 * Math.PI);
-//ctx.fillStyle = "red";
-//ctx.fill();
-ctx.stroke();
+
 
 function init() {
 }
 
 document.onload = init();
 
-btnRun.onclick = function() {
-    if(!running) {
-        if (answered === true) {
-            init();
-            dynamics(1.0 * sliderForce.value);
-            running = true;
-            animate();
-        } else {
-            mustAnswer();
-        }
-    }
-}
+//btnRun.onclick = function() {
+//   if(!running) {
+//      if (answered === true) {
+//            init();
+//            dynamics(1.0 * sliderForce.value);
+//            running = true;
+//            animate();
+//        } else {
+//            mustAnswer();
+//        }
+//    }
+//}
 
 
 function pause() {
@@ -83,7 +84,25 @@ function pause() {
 
 function draw() {
     ctx.save();
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.clearRect(0,0, canvasWidth, canvasHeight);
+    ctx.beginPath();
+    ctx.moveTo(pivoX,pivoY);
+    const radians = sliderAngle.value * (Math.PI / 180.0);
+    xpos = pivoX - sliderLenght.value * 10.0 * Math.sin(radians);
+    ypos = sliderLenght.value * 10.0 * Math.cos(radians);
+    ctx.lineTo(xpos,ypos);
+    ctx.lineWidth = 5;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(xpos, ypos, 20, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'green';
+    /* Black border on the circle */
+    ctx.lineWidth = 2.5; /* Border width */
+    ctx.strokeStyle = 'black'; /* Border color */
+    ctx.fill();
+    /* Draw the border */ 
+    ctx.stroke();  /* It's necessary only if your are drawing the border */   
     ctx.restore();
 }
 
