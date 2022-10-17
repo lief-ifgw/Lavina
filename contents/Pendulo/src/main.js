d8cbd40369c63f39c214868ed207c10449f968c4
ph-moura <phmlinux@gmail.com>
2022-10-17 16:44:00 -0300

    Idea for animation

--- contents/Pendulo/src/main.js
+++ contents/Pendulo/src/main.js
@@ -1,15 +1,16 @@
 /********* Dimensions ************/
 const canvasWidth   = 600;
 const canvasHeight  = 400;
-const pivoX         = canvasWidth / 2.0;  // X position for the pivot
-const pivoY         = 0;    // '' for the Y
-
+const pivo          = new Vector2D(canvasWidth / 2.0, 0)
+const defaultLenght = 25;
+const defaultAngle  = 40;
 /********** Variables ********/
 
 var theta, compL;         // Initial angle and rope lenght
 var ang_freq;             // Oscillation period and angular frequency (to be answered)
 var xpos, ypos, radians;  // Pendulum position parameters
 
+var t = 0.0;
 /********** Physical constants ********/
 
 const alturaH    = 15;       // Fixed height H to the ground.
@@ -24,6 +25,12 @@ canvas.width    = canvasWidth;
 canvas.height   = canvasHeight;
 ctx.font = '18px Arial';
 
+radians = defaultAngle  * (Math.PI / 180.0);
+
+var pos = new Vector2D(pivo.x - defaultLenght * 10.0 * Math.sin(radians), defaultLenght * 10.0 * Math.cos(radians));
+
+var pendulo = new Pendulo(pivo, pos, ang_freq, defaultLenght, defaultAngle);
+
 
 /********** HTML interaction ********/
 
@@ -32,6 +39,7 @@ var sliderAngle  = document.getElementById("angle");
 var lenghtView   = document.getElementById("outLenght");
 var angleView    = document.getElementById("outAngle");
 var omegaView    = document.getElementById("omega");
+var btnRun       = document.getElementById("button-start");
 
 lenghtView.innerHTML = sliderLenght.value;
 angleView.innerHTML  = sliderAngle.value;
@@ -44,27 +52,28 @@ sliderLenght.oninput = function() {
     lenghtView.innerHTML = this.value;
     ang_freq = Math.sqrt(defaultG/sliderLenght.value);
     omegaView.innerHTML = ang_freq;
+    pendulo.setLenght(1.0 * this.value);
     draw();
-
-
 }
 
 
 sliderAngle.oninput = function() {
     angleView.innerHTML = this.value;
+    pendulo.setAngle(1.0 * this.value);
+
     draw();
 }
 
 /********** Animation ********/
+// function init() {
+    // animate();
+// }
 
+// document.onload = init();
 
-
-function init() {
-}
-
-document.onload = init();
-
-//btnRun.onclick = function() {
+btnRun.onclick = function() {
+    console.log("Start!");
+    animate();
 //   if(!running) {
 //      if (answered === true) {
 //            init();
@@ -75,43 +84,21 @@ document.onload = init();
 //            mustAnswer();
 //        }
 //    }
-//}
+}
 
+function draw() {
+    pendulo.draw(ctx);
+}
 
 function pause() {
     cancelAnimationFrame(animId);
 }
 
-function draw() {
-    ctx.save();
-    ctx.clearRect(0,0, canvasWidth, canvasHeight);
-    ctx.beginPath();
-    ctx.moveTo(pivoX,pivoY);
-    const radians = sliderAngle.value * (Math.PI / 180.0);
-    xpos = pivoX - sliderLenght.value * 10.0 * Math.sin(radians);
-    ypos = sliderLenght.value * 10.0 * Math.cos(radians);
-    ctx.lineTo(xpos,ypos);
-    ctx.lineWidth = 5;
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(xpos, ypos, 20, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'green';
-    /* Black border on the circle */
-    ctx.lineWidth = 2.5; /* Border width */
-    ctx.strokeStyle = 'black'; /* Border color */
-    ctx.fill();
-    /* Draw the border */ 
-    ctx.stroke();  /* It's necessary only if your are drawing the border */   
-    ctx.restore();
-}
-
 function animate() {
     animId = requestAnimationFrame(animate);
-    move();
+    t+= 0.02;
+    pendulo.move(t);
     draw();
 }
-
-
 
 draw();
