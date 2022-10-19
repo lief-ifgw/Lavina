5fb7d6092264f8486c52d2421280f2dad380cd8c
PROPedroRibeiro <propedroribeiro@gmail.com>
2022-10-18 23:23:06 -0300

    Modificação na forma que a dinâmica de colisão atua; construção do painel de leitura do LED, bem como o LED na bola.

--- contents/slope/src/script.js
+++ contents/slope/src/script.js
@@ -5,7 +5,6 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
-
 var ctx = canvas.getContext('2d');
 
 let btnStart = document.getElementById("btnStart");
@@ -14,28 +13,37 @@ let running = false;
 let animId;
 let x = 100;
 let y = 100;
-const g = 0.98;
-let friction = 0.1;
+const freq = 15;
+let c = 0;
+const g = 0.098;
+let friction = 0;
 let xcm = 0;
 let ycm = 0;
-let rball = 20;
+let rball = 10;
 let ang = rand(3/2*Math.PI,2*Math.PI);
-let wx2 = canvasWidth - 2*rball;
-let wy2 = canvasHeight - 2*rball;
+let new_rad = Math.abs(ang - 2*Math.PI);
+let wx2 = canvasWidth - 6*rball;
+let wy2 = canvasHeight - 3*rball;
 let wx1 = wx2 - wy2*Math.cos(ang); 
 let wy1 = wy2 + wy2*Math.sin(ang);
 const BALLS = [];
 const WALLS = [];
+let wallBottom;
+let wallLeft;
+let wallRight;
 
-
-let b = new Ball("ball1", wx1+2*rball, wy1+rball, rball, 1, 'black');
-b.elasticity = 0.2;
+let b = new Ball("ball1", wx1 + Math.cos(ang)*rball - rball*Math.sin(ang), wy1 - Math.sin(ang)*rball - rball*Math.cos(ang), rball, 1, 'black');
+b.elasticity = 1;
+b.acceleration = g;
+b.a.y = 1;
 let wall = new Wall(wx1, wy1, wx2, wy2);
 
+document.onload = canvasWall();
 document.onload = mainLoop();
 
+
 function rad_deg(rad){
-    let new_rad = Math.abs(rad - 2*Math.PI);
+    new_rad = Math.abs(rad - 2*Math.PI);
     let deg = (new_rad * 180.0) / Math.PI;
     return deg;
 }
@@ -97,7 +105,6 @@ function closestPointBW(obj1, w1){
     return Vector2D.subtract(w1.start,closestVect);
 }
 
-
 function collisionDetectionWall(obj1,w1){
     let ballClosest = Vector2D.subtract(closestPointBW(obj1,w1),obj1.pos);
     if(ballClosest.length() <= obj1.r){
@@ -105,24 +112,33 @@ function collisionDetectionWall(obj1,w1){
     }
 }
 
-
 function penetrationResultWall(obj1, w1){
     let penVect = Vector2D.subtract(obj1.pos,closestPointBW(obj1, w1));
     obj1.pos = Vector2D.add(obj1.pos, Vector2D.scale(Vector2D.norma(penVect),obj1.r-penVect.length()));
 }
 
-
-function collisionResultWall(obj1, w1){
+function collisionResultWall(obj1, w1){ 
     let normal = Vector2D.norma(Vector2D.subtract(obj1.pos,closestPointBW(obj1, w1)));
     let sepVel = Vector2D.dotProduct(obj1.v, normal);
     let new_sepVel = -sepVel * obj1.elasticity;
     let vsep_diff = sepVel - new_sepVel;
     obj1.v = Vector2D.add(obj1.v,Vector2D.scale(normal,-vsep_diff));
+    obj1.v = Vector2D.scale(obj1.v,1-friction);
 }
 
-function drawCircle(x,y){
+function drawCircle(x,y,r,color){
     ctx.beginPath();
-    ctx.arc(x,y,5,0,2*Math.PI);
+    ctx.arc(x,y,r,0,2*Math.PI);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPanel(){
+    ctx.beginPath();
+    ctx.rect(rball, wy2, canvasWidth - 2*rball, canvasHeight);
     ctx.strokeStyle = 'black';
     ctx.stroke();
     ctx.fillStyle = 'black';
@@ -130,14 +146,15 @@ function drawCircle(x,y){
     ctx.closePath();
 }
 
+
 function mainLoop() {
     ctx.save();
-    ctx.clearRect(0,0,canvasWidth,canvasHeight);
+    c += 1;
+    ctx.clearRect(0,0,canvasWidth,wy2);
     BALLS.forEach((b, index) => {
-        b.drawBall();
-        b.v.y += g;
-        b.a.y = g;
-        
+        c === freq ? b.drawBall('black',false,true) : b.drawBall('black',false,false)  
+        // b.v.y += g;
+        b.reposition();
         WALLS.forEach((w) => {
             w.drawWall();
             if(collisionDetectionWall(BALLS[index], w)){
@@ -146,28 +163,29 @@ function mainLoop() {
             }
         })
         
-        b.reposition();
-        
     });
+
+    if(collisionDetectionWall(b, wallBottom)){
+        pause();
+    }
     
     ctx.beginPath();
-    ctx.arc(wx2,wy2,wy2,Math.PI/2,Math.PI,true);
+    ctx.arc(wx2,wy2,wy2/2,Math.PI,new_rad - Math.PI,false);
     ctx.strokeStyle = 'black';
     ctx.stroke();
     ctx.closePath();
 
+    document.getElementById("vel").innerHTML = Math.abs(round(b.v.y,1));
+
     ctx.restore();
 }
+drawPanel();
 
 function canvasWall(){
-    let wallTop = new Wall(0, 0, canvasWidth, 0);
-    let wallBottom = new Wall(0, canvasHeight, canvasWidth, canvasHeight);
-    let wallLeft = new Wall(0, 0, 0, canvasHeight);
-    let wallRight = new Wall(canvasWidth, 0, canvasWidth, canvasHeight);
+    wallBottom = new Wall(0, wy2, canvasWidth, wy2);
+    wallLeft = new Wall(0, 0, 0, canvasHeight);
+    wallRight = new Wall(canvasWidth, 0, canvasWidth, canvasHeight);
 }
-
-canvasWall();
-
 
 document.getElementById("angle").innerHTML = round(rad_deg(ang),1);
 document.getElementById("wx1").innerHTML = round(wx1,4);
