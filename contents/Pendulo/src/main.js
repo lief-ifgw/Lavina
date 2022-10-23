daeb7d7bbddd1580a0e56c1592f35978eccde94b
MateusCorradini <mateuscorradinilopes@gmail.com>
2022-10-23 13:40:23 -0300

    Ajustes na main: Vínculo dos botões de start e stop do stopwatch + simulação. Correções no index e inicio da animação do pêndulo.

--- contents/Pendulo/src/main.js
+++ contents/Pendulo/src/main.js
@@ -1,20 +1,21 @@
 /********* Dimensions ************/
 const canvasWidth   = 600;
 const canvasHeight  = 400;
-const pivoX         = canvasWidth / 2.0;  // X position for the pivot
-const pivoY         = 0;    // '' for the Y
-
+const pivo          = new Vector2D(canvasWidth / 2.0, 0)
+const defaultLenght = 25;
+const defaultAngle  = 40;
 /********** Variables ********/
 
 var theta, compL;         // Initial angle and rope lenght
-var ang_freq;             // Oscillation period and angular frequency (to be answered)
 var xpos, ypos, radians;  // Pendulum position parameters
 
+var t = 0.0;
 /********** Physical constants ********/
 
 const alturaH    = 15;       // Fixed height H to the ground.
 const defaultG   = 10.0;    // Gravity acceleration
 const osc_period = 1;     // Default oscillation period
+var ang_freq = Math.sqrt(defaultG / defaultLenght);
 
 /********** Canvas building ********/
 
@@ -24,6 +25,12 @@ canvas.width    = canvasWidth;
 canvas.height   = canvasHeight;
 ctx.font = '18px Arial';
 
+radians = defaultAngle  * (Math.PI / 180.0);
+
+var pos = new Vector2D(pivo.x - defaultLenght * 10.0 * Math.sin(radians), defaultLenght * 10.0 * Math.cos(radians));
+
+var pendulo = new Pendulo(pivo, pos, ang_freq, defaultLenght, defaultAngle);
+
 
 /********** HTML interaction ********/
 
@@ -32,6 +39,10 @@ var sliderAngle  = document.getElementById("angle");
 var lenghtView   = document.getElementById("outLenght");
 var angleView    = document.getElementById("outAngle");
 var omegaView    = document.getElementById("omega");
+var btnRun       = document.getElementById("button-start");
+var btnStop      = document.getElementById("button-stop");
+var t_sec        = document.getElementById("seconds");
+
 
 lenghtView.innerHTML = sliderLenght.value;
 angleView.innerHTML  = sliderAngle.value;
@@ -44,27 +55,31 @@ sliderLenght.oninput = function() {
     lenghtView.innerHTML = this.value;
     ang_freq = Math.sqrt(defaultG/sliderLenght.value);
     omegaView.innerHTML = ang_freq;
+    pendulo.setLenght(1.0 * this.value);
+    pendulo.setAngularFrequency(ang_freq);
     draw();
-
-
 }
 
 
 sliderAngle.oninput = function() {
     angleView.innerHTML = this.value;
+    pendulo.setAngle(1.0 * this.value);
+
     draw();
 }
 
+
+
 /********** Animation ********/
+// function init() {
+    // animate();
+// }
 
-
-
-function init() {
-}
-
-document.onload = init();
+// document.onload = init();
 
 //btnRun.onclick = function() {
+//    console.log("Start!");
+//    animate();
 //   if(!running) {
 //      if (answered === true) {
 //            init();
@@ -77,41 +92,95 @@ document.onload = init();
 //    }
 //}
 
+//btnStop.onclick = function() {
+
+//    pause();
+//    ctx.clearRect(0,0, canvasWidth, canvasHeight);
+//    draw();
+//}
+
+// STOPWATCH STUFF
+
+window.onload = function () {
+
+  var seconds = 00;
+  var tens = 00;
+  var appendTens = document.getElementById("tens")
+  var appendSeconds = document.getElementById("seconds")
+  //var buttonStart = document.getElementById('button-start');
+  //var buttonStop = document.getElementById('button-stop');
+  var buttonReset = document.getElementById('button-reset');
+  var Interval ;
+
+  btnRun.onclick = function() {
+     console.log("Start!");
+     //animate();
+    clearInterval(Interval);
+     Interval = setInterval(startTimer, 10);
+     animate();
+  }
+
+    btnStop.onclick = function() {
+        pause();
+        draw();
+       clearInterval(Interval);
+  }
+
+
+  buttonReset.onclick = function() {
+     clearInterval(Interval);
+    tens = "00";
+  	seconds = "00";
+    appendTens.innerHTML = tens;
+  	appendSeconds.innerHTML = seconds;
+  }
+
+
+
+  function startTimer () {
+    tens++;
+
+    if(tens <= 9){
+      appendTens.innerHTML = "0" + tens;
+    }
+
+    if (tens > 9){
+      appendTens.innerHTML = tens;
+
+    }
+
+    if (tens > 99) {
+      console.log("seconds");
+      seconds++;
+      appendSeconds.innerHTML = "0" + seconds;
+      tens = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+
+    if (seconds > 9){
+      appendSeconds.innerHTML = seconds;
+    }
+
+  }
+
+
+}
+
+
+function draw() {
+    pendulo.draw(ctx);
+}
 
 function pause() {
     cancelAnimationFrame(animId);
 }
 
-function draw() {
-    ctx.save();
-    ctx.clearRect(0,0, canvasWidth, canvasHeight);
-    ctx.beginPath();
-    ctx.moveTo(pivoX,pivoY);
-    const radians = sliderAngle.value * (Math.PI / 180.0);
-    xpos = pivoX - sliderLenght.value * 10.0 * Math.sin(radians);
-    ypos = sliderLenght.value * 10.0 * Math.cos(radians);
-    ctx.lineTo(xpos,ypos);
-    ctx.lineWidth = 5;
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.arc(xpos, ypos, 20, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'green';
-    /* Black border on the circle */
-    ctx.lineWidth = 2.5; /* Border width */
-    ctx.strokeStyle = 'black'; /* Border color */
-    ctx.fill();
-    /* Draw the border */ 
-    ctx.stroke();  /* It's necessary only if your are drawing the border */   
-    ctx.restore();
-}
-
 function animate() {
     animId = requestAnimationFrame(animate);
-    move();
+    t += 0.1;
+    //t += t_sec.value;
+    pendulo.move(t);
     draw();
 }
-
-
 
 draw();
