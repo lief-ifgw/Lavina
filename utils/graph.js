816c4bd7313275e1bb546bf3ecb4f9046ab217ee
PROPedroRibeiro <propedroribeiro@gmail.com>
2023-06-05 13:39:07 -0300

    Reestruturação do código para a construção de gráficos

--- utils/graph.js
+++ utils/graph.js
@@ -2,12 +2,9 @@ class Graph{
     constructor(canvas){ 
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
-        let mouse_down;
-        let mouse_up;
-        let mouse_move;
     }
 
-    drawGrid(px, py, color,thickGrid=0.9){
+    drawGrid(px, color,thickGrid=0.9){
         setLine(oX,0,oX,this.canvas.height,'black',this.canvas,thickGrid*1.5); //linha eixo y
         setLine(0,oY,this.canvas.width,oY,'black',this.canvas,thickGrid*1.5); //linha eixo x
         for(let x = -this.canvas.width-oX;x<this.canvas.width-oX;x += 1){//linhas y
@@ -16,10 +13,12 @@ class Graph{
             }
         }
         for(let y = -this.canvas.height-oY;y<this.canvas.height-oY;y += 1){//linhas x
-            if(y%py == 0){
+            if(y%px == 0){
                 setLine(0,oY+y,this.canvas.width,oY+y,color,this.canvas,thickGrid);
             } 
         }
+        this.ctx.font = "italic 25px Times";
+        this.ctx.fillText("tempo (s)",canvasWidth-(5*px),canvasHeight-(2*px));
     }
 
     drawPoint(x,y,thick = 2){
@@ -28,70 +27,19 @@ class Graph{
         this.ctx.fill();
     }
 
-    plotFunction(px,py,FUNCT,COLORFUNCT,colorGrid="grey",thick = 2, insta = true,canvas = this.canvas,x){
-        this.drawGrid(px,py,colorGrid);
-        // let x = -oX;
+    plotFunction(px,funct,colorGrid="grey"){
+        this.drawGrid(px,colorGrid);
+        let x;
         let y;
-        let index = 0;
-        let j = 0;
-        let xAntes;
-        let yAntes;
-        let fTransf = [];
-        FUNCT.forEach((f) => {
-            f = f.replace(/X/g,"(x/px)");
-            f = "(" + f + ")*py";
-            fTransf.push(f);
-        });
-        FUNCT = fTransf;
-        if(insta){
-            FUNCT.forEach((f,i) => {
-                for(x=-oX;x<canvas.width-oX;x += px/100){
-                    y = eval(f)*(py/px);
-                    x == -oX ? {}: setLine(xAntes+oX,-yAntes+oY,x+oX,-y+oY,COLORFUNCT[i],canvas,thick);   
-                    xAntes = x;
-                    yAntes = y;
-                }
-            });
-        }
-        else{
-            let y = eval(FUNCT[index])*(py/px);
-            let xAntes = x;
-            let yAntes = y; 
-            if(x !== -oX){
-                setLine(xAntes+oX,-yAntes+oY,x+oX,-y+oY,COLORFUNCT[index],canvas,thick);
-            }
-            //x += px/100;
-            if(x >= canvas.width-oX){
-                index += 1;
-                x = -oX;
-            }
-            return{index,x};
-
+        let xAntes = 0;
+        let yAntes = 0;
+        for(x=-oX;x<this.canvas.width-oX;x += px/100){
+            y = eval(funct.expression)
+            x == -oX ? this.drawPoint(x+oX,(-y)+oY,thick): setLine(xAntes+oX,-yAntes+oY,x+oX,-y+oY,funct.color,this.canvas,thick);   
+            xAntes = x;
+            yAntes = y;
         }
     }
-    fu(f,x,i){
-        let y = eval(f)*(py/px);
-        let xAntes = x;
-        let yAntes = y;
-        console.log(x); 
-        x == -oX ? this.drawPoint(x+oX,(-y)+oY,thick): setLine(xAntes+oX,-yAntes+oY,x+oX,-y+oY,COLORFUNCT[i],this.canvas,thick);   
-        x += px/100;
-        console.log(x);
-        if(x >= this.canvas.width-oX){
-            clearInterval(sett);
-        }
-    }
-    
-
-    // var counter = 0;
-    // var i = setInterval(function(){
-    //     // do your thing
-    
-    //     counter++;
-    //     if(counter === 10) {
-    //         clearInterval(i);
-    //     }
-    // }, 200);
 
     moveGraph(){    
         this.canvas.onmousedown = mouse_down;
@@ -100,6 +48,44 @@ class Graph{
         this.canvas.onmouseout = mouse_up;
     }
     
+    coordGraph(xCoord,yCoord){
+        let canvas = this.canvas;
+        function getMouseCoord(canvas, evt) {
+            var rect = canvas.getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left - oX,
+                y: -1*(evt.clientY - Math.round(rect.top) - oY)
+            };
+        }
+        function coordCalcs(evt){
+            var mousePos = getMouseCoord(canvas, evt);
+            xCoord.innerHTML = Math.round((((mousePos.x)/px)*100)*xScale)/100;
+            yCoord.innerHTML = Math.round((((mousePos.y)/px)*100)/yScale)/100;
+        }
+        canvas.addEventListener("mousemove",coordCalcs);
+    }
+
+    zoom(btnZoomIn,btnZoomOut){
+        btnZoomIn.onclick = function(){
+            if(px < 500){
+                px += 10;
+                thick += 0.2;
+            }
+            else{
+                
+            }
+        }
+        btnZoomOut.onclick = function(){
+            if(px >= 10){
+                px -= 10;
+                thick -= 0.2;
+            }
+            else{
+                
+            }
+        }
+    }
+
 }
 setLine = function(x0,y0,xf,yf,color,canvas,lw=1){
     let ctx = canvas.getContext('2d');
@@ -143,6 +129,5 @@ let mouse_move = function(event){
         oY += dy;
         startX = mouseX;
         startY = mouseY;
-        console.log(oX,oY);
     }
 }
