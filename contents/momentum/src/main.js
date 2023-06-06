a02dbb848f7e63dbc7ea5c48e59960a34d746715
Mateus Corradini Lopes <64249204+MateusCorradini@users.noreply.github.com>
2023-06-05 23:24:17 -0300

    update 05/06 - bug fix
    Correção de bug na definição dos movimentos.

--- contents/momentum/src/main.js
+++ contents/momentum/src/main.js
@@ -204,12 +204,14 @@ function animate() {
     //Elastica (teste)
     if( Math.abs(ball1.pos.x - ball2.pos.x) < 20 ){
       //v1f = new Vector2D(0.005*t*((-10.0 - sliderV1.value) + (2.0*sliderM1.value*sliderV1.value + (-10.0)**2 * (sliderM2.value - sliderM1.value))/(sliderM2.value - sliderM1.value)),0);
-      var m1 = sliderM1.value;
-      var m2 = sliderM2.value;
-      var v1 = sliderV1.value;
-      ball1.setV( 50.0*(((m1 - m2)/(m1 + m2))*v1 + (2.0*m2/(m1+m2))*(-10.0)));
+      var m1 = parseInt(sliderM1.value);
+      var m2 = parseInt(sliderM2.value);
+      var v1 = parseInt(sliderV1.value);
+      var testev =  ((((m1 - m2)*v1)/(m1 + m2))) + (((2.0*m2*(-10.0))/(m1+m2)));
+      console.log("%f %f %f %f",m1,m2,v1,testev);
+      ball1.setV((((m1 - m2)/(m1 + m2))*v1 + (2.0*m2/(m1+m2))*(-10.0)));
       //console.log("%f",50.0*(((m1 - m2)/(m1 + m2))*v1 + (2.0*m2/(m1+m2))*(-10.0)));
-      ball2.setV( 50.0*(((2.0*m1)/(m1+m2))*v1 + ((m2 - m1)/(m1+m2))*(-10.0)));
+      ball2.setV((((2.0*m1)/(m1+m2))*v1 + ((m2 - m1)/(m1+m2))*(-10.0)));
       //ball1.move1(t,sliderM1.value,sliderM2.value,sliderV1.value);
       //ball2.move2(t,sliderM1.value,sliderM2.value,sliderV1.value);
     }
