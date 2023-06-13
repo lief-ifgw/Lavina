c9cb9071644d360cfa14fa41954ee240e9d3ff7a
PROPedroRibeiro <propedroribeiro@gmail.com>
2023-06-13 11:52:22 -0300

    Organização do código; reajuste para evitar código desnecessário; inserção de legendas para o plano

--- contents/center-mass/src/script.js
+++ contents/center-mass/src/script.js
@@ -1,3 +1,6 @@
+/////////////////////////////////
+/*         CANVAS INFO         */
+
 const canvasWidth = 600;
 const canvasHeight = canvasWidth * 2 / 3;
 
@@ -6,6 +9,9 @@ canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 var ctx = canvas.getContext('2d');
+
+/////////////////////////////////
+/*      ATRIBUIÇÃO DE IDS      */
 
 let color = ['#5050FF','#FF5050','yellow'];
 let displayCM  = document.getElementById("displayCM");
@@ -31,22 +37,33 @@ let ansXCM = document.getElementById("ansXCM");
 let ansYCM = document.getElementById("ansYCM");
 let ansCM = document.getElementById("ansCM");
 
+/////////////////////////////////
+/*   DECLARAÇÃO DE VARIÁVEIS   */
+
 let x = 100;
 let y = 100;
 let k = 0;
+let rball = 15;
 let friction = 0.1;
 let xcm = 0;
 let ycm = 0;
-let gridX = 30;
-let gridY = 30;
+let px = 30;
+let oX = 3*px;
+let oY = canvasHeight - 3*px;
+let labelX = "x (m)";
+let labelY = "y (m)";
 let clicked;
 const BALLS = [];
 const MASS = [];
+
+/////////////////////////////////
+/*      CLASSES E FUNÇÕES      */
 
 function round(n, precision){
     let factor = 10**precision;
     return Math.round(n*factor)/factor;
 }
+
 
 function setLine(x0,y0,xf,yf,color,lw=1){
     ctx.beginPath();    
@@ -56,45 +73,6 @@ function setLine(x0,y0,xf,yf,color,lw=1){
     ctx.lineWidth = lw;
     ctx.stroke();
     ctx.closePath();
-}
-
-function drawGrid(px,py,color){
-    s = 2;
-    pex = px;
-    pey = py;
-    xSpaces = canvasWidth/px;
-    ySpaces = canvasHeight/py;
-    for(i = 1; i < xSpaces; i++){
-        if(i === s){
-            setLine(px*i,0,px*i,canvasHeight,'black',3);    
-        }
-        else{
-            setLine(px*i,0,px*i,canvasHeight,color);
-        }
-    }
-    for(i = 1; i < ySpaces; i++){
-        if(i === Math.round(ySpaces) - s){
-            setLine(0,py*i,canvasWidth,py*i,'black',3);    
-        }
-        else{
-            setLine(0,py*i,canvasWidth,py*i,color);
-        }
-    }
-}
-
-function massCenter(){
-    xcm = 0;
-    ycm = 0;
-    for(i = 0; i < BALLS.length; i++){
-        if(MASS.length <= 2){MASS.push(BALLS[i].m);}
-        xcm += BALLS[i].m * BALLS[i].pos.x;
-        ycm += BALLS[i].m * BALLS[i].pos.y;
-    }
-    let totalMass = MASS.reduce((p,c) => p + c, 0)
-    xcm = xcm/totalMass;
-    ycm = ycm/totalMass;
-    totalMass = 0;
-
 }
 
 function drawX(cx, cy, l, t = 1){
@@ -113,62 +91,18 @@ function drawX(cx, cy, l, t = 1){
     ctx.closePath();
 }
 
-let ball1 = new Ball("ball1", Math.random()*canvasWidth, Math.random()*canvasHeight, 15, Math.ceil(Math.random()*9));
-let ball2 = new Ball("ball2", Math.random()*canvasWidth, Math.random()*canvasHeight, 15, Math.ceil(Math.random()*9));
-let ball3 = new Ball("ball3", Math.random()*canvasWidth, Math.random()*canvasHeight, 15, Math.ceil(Math.random()*9));
-
-
-drawGrid(gridX,gridY,'blue');
-BALLS.forEach((b,index) => {
-    b.drawBall(canvas,color[index],true);
-});
-
-massCenter();
-
-
-let xcm0 = round(xcm - pex * s, 0);
-let ycm0 = round(-1 * ycm + pey * (Math.round(ySpaces)-s), 0);
-
-let arrCMX = [xcm0-Math.ceil(Math.random()*10),xcm0+Math.ceil(Math.random()*10),xcm0];
-let arrCMY = [ycm0-Math.ceil(Math.random()*10),ycm0+Math.ceil(Math.random()*10),ycm0];
-let arrCM = ["X:"+arrCMX[0]+", Y:"+arrCMY[0],"X:"+arrCMX[1]+", Y:"+arrCMY[1],"X:"+arrCMX[2]+", Y:"+arrCMY[2]];
-arrCM = arrCM.sort(() => Math.random() - 0.5)
-let ball1x = round(ball1.pos.x - pex * s, 0);
-let ball2x = round(ball2.pos.x - pex * s, 0);
-let ball3x = round(ball3.pos.x - pex * s, 0);
-let ball1y = round(-1 * ball1.pos.y + pey * (Math.round(ySpaces)-s), 0); 
-let ball2y = round(-1 * ball2.pos.y + pey * (Math.round(ySpaces)-s), 0);
-let ball3y = round(-1 * ball3.pos.y + pey * (Math.round(ySpaces)-s), 0);
-let ball1m = ball1.m;
-let ball2m = ball2.m;
-let ball3m = ball3.m;
-let sumBallx = ball1x * ball1m + ball2x * ball2m + ball3x * ball3m;
-let sumBally = ball1y * ball1m + ball2y * ball2m + ball3y * ball3m;
-let sumMass = ball1m + ball2m + ball3m;
-ball1X.innerHTML = ball1x;
-ball2X.innerHTML = ball2x;
-ball3X.innerHTML = ball3x;
-ball1Y.innerHTML = ball1y;
-ball2Y.innerHTML = ball2y;
-ball3Y.innerHTML = ball3y;
-ball1M.innerHTML = ball1m;
-ball2M.innerHTML = ball2m;
-ball3M.innerHTML = ball3m;
-r1.innerHTML = arrCM[0];
-r2.innerHTML = arrCM[1];
-r3.innerHTML = arrCM[2];
-
-ansXCM.innerHTML = "$\\LARGE{x_{cm} = \\frac{("+ball1x+")*("+ball1m+")+("+ball2x+")*("+ball2m+")+("+ball3x+")*("+ball3m+")}{"+ball1m+"+"+ball2m+"+"+ball3m+"}=\\frac{"+sumBallx+"}{"+sumMass+"}\\approx "+xcm0+"}$";
-ansYCM.innerHTML = "$\\LARGE{y_{cm} = \\frac{("+ball1y+")*("+ball1m+")+("+ball2y+")*("+ball2m+")+("+ball3y+")*("+ball3m+")}{"+ball1m+"+"+ball2m+"+"+ball3m+"}=\\frac{"+sumBally+"}{"+sumMass+"}\\approx "+ycm0+"}$";
-ansCM.innerHTML = "$\\large{("+xcm0+","+ycm0+")}$";
-
-function isAnswered() {
-    if (opt1.checked === true || opt2.checked === true || opt3.checked === true) {
-        return true;
+function massCenter(){
+    xcm = 0;
+    ycm = 0;
+    for(i = 0; i < BALLS.length; i++){
+        if(MASS.length <= 2){MASS.push(BALLS[i].m);}
+        xcm += BALLS[i].m * BALLS[i].pos.x;
+        ycm += BALLS[i].m * BALLS[i].pos.y;
     }
-    else{
-        return false;
-    }
+    let totalMass = MASS.reduce((p,c) => p + c, 0)
+    xcm = xcm/totalMass;
+    ycm = ycm/totalMass;
+    totalMass = 0;
 }
 
 function cleanAnswer() {
@@ -181,9 +115,16 @@ function mustAnswer() {
     window.alert("Você deve marcar uma opção para ver o resultado.");
 }
 
- cleanAnswer();
+function isAnswered() {
+    if (opt1.checked === true || opt2.checked === true || opt3.checked === true) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
- displayCM.onclick = function() {
+displayCM.onclick = function() {
     if (isAnswered()) {
         clicked = true;
         drawX(xcm,ycm,20,7);   
@@ -235,3 +176,53 @@ answerCM.addEventListener("click", function() {
         mustAnswer();
     }
 });
+
+/////////////////////////////////
+/* CHAMADA DE MÉTODOS E OBJETOS*/ 
+
+let graph1 = new Graph(canvas);
+graph1.drawGrid(px,"lightgrey",1,oX,oY,labelX,labelY);
+let ball1 = new Ball("ball1", rball+Math.random()*(canvasWidth-(2*rball)), rball+Math.random()*(canvasHeight-(2*rball)), rball, Math.ceil(Math.random()*9));
+let ball2 = new Ball("ball2", rball+Math.random()*(canvasWidth-(2*rball)), rball+Math.random()*(canvasHeight-(2*rball)), rball, Math.ceil(Math.random()*9));
+let ball3 = new Ball("ball3", rball+Math.random()*(canvasWidth-(2*rball)), rball+Math.random()*(canvasHeight-(2*rball)), rball, Math.ceil(Math.random()*9));
+
+BALLS.forEach((b,index) => {
+    b.drawBall(canvas,color[index],true);
+});
+massCenter();
+
+let xcm0 = round(xcm - oX, 0);
+let ycm0 = round((-1 * ycm) + oY, 0);
+let arrCMX = [xcm0-Math.ceil(Math.random()*10),xcm0+Math.ceil(Math.random()*10),xcm0];
+let arrCMY = [ycm0-Math.ceil(Math.random()*10),ycm0+Math.ceil(Math.random()*10),ycm0];
+let arrCM = ["X:"+arrCMX[0]+", Y:"+arrCMY[0],"X:"+arrCMX[1]+", Y:"+arrCMY[1],"X:"+arrCMX[2]+", Y:"+arrCMY[2]];
+arrCM = arrCM.sort(() => Math.random() - 0.5)
+let ball1x = round(ball1.pos.x - oX, 0);
+let ball3x = round(ball3.pos.x - oX, 0);
+let ball2x = round(ball2.pos.x - oX, 0);
+let ball1y = round((-1 * ball1.pos.y) + oY, 0); 
+let ball2y = round((-1 * ball2.pos.y) + oY, 0);
+let ball3y = round((-1 * ball3.pos.y) + oY, 0);
+let ball1m = ball1.m;
+let ball2m = ball2.m;
+let ball3m = ball3.m;
+let sumBallx = ball1x * ball1m + ball2x * ball2m + ball3x * ball3m;
+let sumBally = ball1y * ball1m + ball2y * ball2m + ball3y * ball3m;
+let sumMass = ball1m + ball2m + ball3m;
+ball1X.innerHTML = ball1x;
+ball2X.innerHTML = ball2x;
+ball3X.innerHTML = ball3x;
+ball1Y.innerHTML = ball1y;
+ball2Y.innerHTML = ball2y;
+ball3Y.innerHTML = ball3y;
+ball1M.innerHTML = ball1m;
+ball2M.innerHTML = ball2m;
+ball3M.innerHTML = ball3m;
+r1.innerHTML = arrCM[0];
+r2.innerHTML = arrCM[1];
+r3.innerHTML = arrCM[2];
+ansXCM.innerHTML = "$\\LARGE{x_{cm} = \\frac{("+ball1x+")*("+ball1m+")+("+ball2x+")*("+ball2m+")+("+ball3x+")*("+ball3m+")}{"+ball1m+"+"+ball2m+"+"+ball3m+"}=\\frac{"+sumBallx+"}{"+sumMass+"}\\approx "+xcm0+"}$";
+ansYCM.innerHTML = "$\\LARGE{y_{cm} = \\frac{("+ball1y+")*("+ball1m+")+("+ball2y+")*("+ball2m+")+("+ball3y+")*("+ball3m+")}{"+ball1m+"+"+ball2m+"+"+ball3m+"}=\\frac{"+sumBally+"}{"+sumMass+"}\\approx "+ycm0+"}$";
+ansCM.innerHTML = "$\\large{("+xcm0+","+ycm0+")}$";
+cleanAnswer();
+
